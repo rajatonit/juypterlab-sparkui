2a8de96ea1c764c5f175172fc55e258511d58ac9
Rajat Arora <rajat.arora@hotmail.ca>
2021-09-26 20:20:47 -0400

    kinda got it working

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { ICommandPalette } from '@jupyterlab/apputils';
+import { ICommandPalette, IFrame } from '@jupyterlab/apputils';
+import { PageConfig } from '@jupyterlab/coreutils';
+
+import { Widget } from '@lumino/widgets';
+
 import { requestAPI } from './handler';
 
 /**
@@ -16,7 +20,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
   requires: [ICommandPalette],
   activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('JupyterLab extension juypterlab-sparkui-tab is activated!');
-    const { commands } = app;
+    const { commands, shell } = app;
 
     // Add a command
     const command = 'spark:open-spark-ui';
@@ -28,10 +32,17 @@ const plugin: JupyterFrontEndPlugin<void> = {
         console.log(
           `jlab-examples:main-menu has been called ${args['origin']}.`
         );
+        const sparkWidget = new SparkUI();
+        sparkWidget.title.label = 'Open Spark UI';
 
         requestAPI<any>('ui')
           .then(data => {
             console.log(data);
+            const widget = new sparkUIWidget(data);
+            widget.id = 'sparkui-jupyterlab';
+            widget.title.label = 'SparkUI';
+            widget.title.closable = true;
+            shell.add(widget, 'main');
           })
           .catch(reason => {
             console.error(
@@ -45,7 +56,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
     });
 
     // Add the command to the command palette
-    const category = 'Spark';
+    const category = 'SparkUI';
     palette.addItem({
       command,
       category,
@@ -54,22 +65,39 @@ const plugin: JupyterFrontEndPlugin<void> = {
   }
 };
 
-// const plugin: JupyterFrontEndPlugin<void> = {
-//   id: 'juypterlab-sparkui-tab:plugin',
-//   autoStart: true,
-//   activate: (app: JupyterFrontEnd) => {
-//     console.log('JupyterLab extension juypterlab-sparkui-tab is activated!');
+class SparkUI extends IFrame {
+  // html: string;
+  constructor() {
+    super();
+    const baseUrl = PageConfig.getBaseUrl();
+    this.url = baseUrl + 'sparkuitab/';
+  }
+}
 
-//     requestAPI<any>('ui')
-//       .then(data => {
-//         console.log(data);
-//       })
-//       .catch(reason => {
-//         console.error(
-//           `The juypterlab_sparkui_tab server extension appears to be missing.\n${reason}`
-//         );
-//       });
-//   }
-// };
+class sparkUIWidget extends Widget {
+  /**
+  * Construct a new APOD widget.
+  */
+  constructor(html:any) {
+    super();
+
+    this.addClass('sparkui-widget');
+
+    // Add an image element to the panel
+    this.iframe = document.createElement('iframe');
+    // const baseUrl = PageConfig.getBaseUrl();
+    this.iframe.setAttribute("srcdoc", html);
+    this.iframe.setAttribute("width", "100%");
+    this.iframe.setAttribute("height", "100%");
+
+    this.node.appendChild(this.iframe);
+  }
+
+  /**
+  * The summary text element associated with the widget.
+  */
+  readonly iframe: HTMLElement;
+
+}
 
 export default plugin;
